a3a2dbab16d1c86de3b882b2fee1517d8fe9da5e
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-01-24 21:08:43 +0100

    Stuff

--- server/main.ts
+++ server/main.ts
@@ -1,1 +1,10 @@
-console.log('server');
+import { Server } from 'colyseus';
+import { createServer } from 'http';
+
+const port = parseInt(process.env.PORT!) || 3000;
+
+const gameServer = new Server({
+  server: createServer(),
+}).listen(port);
+
+console.log(`server listening on localhost:${port}`);
