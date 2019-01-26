9608b2317189cb7fba0972112e966a4c3635c721
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-01-26 18:09:44 +0100

    Minimal server

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,33 @@
-import { Server } from 'colyseus';
+import { Client, Room, Server } from 'colyseus';
 import { createServer } from 'http';
 
-const port = parseInt(process.env.PORT!) || 3000;
+import { MainRoom } from './main-room';
+
+const port = parseInt(process.env.PORT!) || 2567;
 
 const gameServer = new Server({
   server: createServer(),
-}).listen(port);
-
+});
+gameServer.listen(port);
 console.log(`server listening on localhost:${port}`);
+
+(async () => {
+  const handler = await gameServer.register('main', MainRoom);
+  handler
+    .on('create', (room: Room) => console.log('room created:', room.roomId))
+    .on('dispose', (room: Room) => console.log('room disposed:', room.roomId))
+    .on('join', (room: Room, client: Client) =>
+      console.log(client.id, 'joined', room.roomId),
+    )
+    .on('leave', (room: Room, client: Client) =>
+      console.log(client.id, 'left', room.roomId),
+    );
+})().catch(console.error);
+
+setInterval(
+  () =>
+    gameServer.matchMaker
+      .getAllRooms('main')
+      .then((rooms) => console.log(`all 'main' rooms:`, rooms)),
+  10e3,
+);
