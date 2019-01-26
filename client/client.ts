891efe4d2b9ff485cec409f6281ff2fccc6eed4e
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-01-26 18:05:03 +0100

    minimal client

--- client/client.ts
+++ client/client.ts
@@ -1,1 +1,22 @@
-console.log('client');
+import { Client } from 'colyseus.js';
+
+const client = new Client('ws:localhost:2567');
+
+client.onError.add(console.error);
+client.onMessage.add(console.log);
+client.onOpen.add(console.log);
+
+client.getAvailableRooms('main', (rooms, err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log({ rooms });
+  rooms.forEach((room) => {
+    console.log(room.roomId);
+    console.log(room.clients);
+    console.log(room.maxClients);
+    console.log(room.metadata);
+  });
+});
+
+const room = client.join('main');
